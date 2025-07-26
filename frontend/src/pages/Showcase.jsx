c752421912ad0b7f94602392150d25eafd08831a
Matteo DeSimone <thematteod@gmail.com>
2025-07-26 15:12:16 -0400

    Starts populating showcase with mock data

--- frontend/src/pages/Showcase.jsx
+++ frontend/src/pages/Showcase.jsx
@@ -1,7 +1,45 @@
+import { useState, useEffect } from 'react';
+
 export default function Showcase() {
+    const [projects, setProjects] = useState([]);
+
+    const mockProjects = [
+        {
+            id: "1",
+            title: "Project 1",
+            description: "Project 1's description.",
+            image: null,
+            tags: ["Python", "Flask", "React"]
+        },
+        {
+            id: "2",
+            title: "Project 2",
+            description: "Project 2's description",
+            image: null,
+            tags: ["C#", ".Net", "Angular"]
+        }
+    ];
+
+    useEffect(() => {
+        setProjects(mockProjects); 
+    }, []);
+
+
     return (
       <>
         <h1>Showcase Page</h1>
+
+
+        {projects.length > 0 && (
+        <div>
+            <h2>{projects[0].title}</h2>
+            <p>{projects[0].description}</p>
+            <ul>
+            {projects[0].tags.map(tag => <li key={tag}>{tag}</li>)}
+            </ul>
+        </div>
+        )}
+
       </>  
     );
 }
