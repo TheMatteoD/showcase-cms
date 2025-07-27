3870fdff841888208971b6573312daebb4f54b91
Matteo DeSimone <thematteod@gmail.com>
2025-07-27 17:16:52 -0400

    Starts populating the about me page

---
+++ backend/api/get-about-me.php
@@ -0,0 +1,12 @@
+<?php
+    require_once('../cors.php');
+    header('Content-Type: application/json');
+
+    $dbFile = __DIR__ . '/../data/showcase.db';
+    $db = new SQLite3($dbFile);
+
+    $query = "SELECT * FROM about_me LIMIT 1";
+    $results = $db->query($query)->fetchArray();
+
+    echo json_encode($results);
+?>
--- frontend/src/pages/AboutMe.jsx
+++ frontend/src/pages/AboutMe.jsx
@@ -1,7 +1,24 @@
+import { useEffect, useState } from "react";
+
 export default function AboutMe() {
-    return (
+  const [info, setInfo] = useState();
+  
+  useEffect(() => {
+    fetch(`${process.env.REACT_APP_API_BASE_URL}/api/get-about-me.php`)
+    .then(res => res.json())
+    .then(data => setInfo(data))
+    .catch(err => console.error(err));
+  }, []);
+
+  if (!info) return <p>Loading...</p>;
+
+  return (
       <>
         <h1>About Me Page</h1>
+
+        <h3>{info.name}</h3>
+
+        <p>{info.bio}</p>
       </>  
     );
 }
